94065a4600a142b82f6b84222800dc1f520a6492
Okkar Min <omin001@e.ntu.edu.sg>
2021-06-19 16:52:17 +0800

    Add comments

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -7,30 +7,34 @@ const Slug = () => {
 };
 
 export const getServerSideProps: GetServerSideProps = async (context: any) => {
+  // get the domain.com/{slug} into slug variable
   const { slug } = context.params;
 
+  // replace with values from Firebase project
   const firebaseConfig = {
     apiKey: "{ value }",
     authDomain: "{ value }",
     projectId: "{ value }",
   };
 
+  // we are checking if firebase has been initialized before,
+  // if yes, then we don't re-initialize
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
   }
   const db = firebase.firestore();
 
+  // query Firestore
   let result: { slug: string; full_url: string } | undefined;
-
   const querySnapShot = await db
     .collection("urls")
     .where("slug", "==", slug)
     .get();
-
   querySnapShot.forEach(
     (doc) => (result = doc.data() as { slug: string; full_url: string })
   );
 
+  // return redirect url based on query result
   return result == undefined
     ? {
         redirect: {
